b40c4aa9aee8847981f632182640b5e8dfea392f
shisx <shoushizhe@gmail.com>
2026-07-08 11:13:05 +0800

    增强大文件导入上传稳定性

--- src/main/java/com/danganguan/archive/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/danganguan/archive/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.danganguan.archive.common.exception;
 
 import com.danganguan.archive.common.response.Result;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +11,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BizException.class)
     public Result<Void> handleBizException(BizException ex) {
         return Result.fail(ex.getMessage());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public Result<Void> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException ex) {
+        return Result.fail("上传文件过大，请压缩后重试或改用直传/分片上传");
     }
 
     @ExceptionHandler(Exception.class)
